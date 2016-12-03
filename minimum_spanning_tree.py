efc0594cf64a57748115c31a126ed7cb0eb5f66d
Salvador Fuentes <salvador.fga@gmail.com>
2016-12-02 23:07:12 -0600

    final version
    Signed-off-by: Salvador Fuentes <salvador.fga@gmail.com>

--- minimum_spanning_tree.py
+++ minimum_spanning_tree.py
@@ -9,63 +9,58 @@ from pylab import show
 import numpy
 import re
 
-a = [0, 289, 549, 195, 331, 206, 220, 233, 333, 383] # GDL
-b = [0, 0, 310, 448, 0, 0, 314, 197, 336, 330]     # MORELIA
-c = [0, 0, 0, 0, 0, 0, 574, 408, 418, 238]        # CDMX
+# Connections matrix
+a = [0, 289, 549, 195, 0, 206, 220, 233, 333, 0] # GDL
+b = [0, 0, 310, 448, 0, 0, 314, 197, 336, 330]   # MORELIA
+c = [0, 0, 0, 0, 0, 0, 574, 408, 418, 238]       # CDMX
 d = [0, 0, 0, 0, 378, 0, 0, 0, 0, 0]             # COLIMA
-e = [0, 0, 0, 0, 0, 162, 0, 0, 0, 0]            # VALLARTA
-f = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]              # TEPIC
-g = [0, 0, 0, 0, 0, 0, 0, 124, 166, 0]         # AGS
-h = [0, 0, 0, 0, 0, 0, 0, 0, 181, 168]       # LEON
-i = [0, 0, 0, 0, 0, 0, 0, 0, 0, 212]       # SLP
-j = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]         # QUERETARO
+e = [0, 0, 0, 0, 0, 162, 0, 0, 0, 0]             # VALLARTA
+f = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]               # TEPIC
+g = [0, 0, 0, 0, 0, 0, 0, 124, 166, 0]           # AGS
+h = [0, 0, 0, 0, 0, 0, 0, 0, 181, 168]           # LEON
+i = [0, 0, 0, 0, 0, 0, 0, 0, 0, 212]             # SLP
+j = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]               # QUERETARO
 
 X = csr_matrix([a, b, c, d, e, f, g, h, i, j])
 
 print 'All available Connections:'
 print X.toarray().astype(int)
 
-str_repr= str(X)
-mylist = []
-
+# Get tuples for drawing the edges
 def getTuples( mymatrix ):
     str_repr= str( mymatrix )
     mylist = []
     for line in str_repr.splitlines():
         m = re.search('\((.+?)\)', line)
         mytuple = m.group(1)
-    
-        a= int(mytuple.split(', ', 1)[0])
-        b= int(mytuple.split(', ', 1)[1])
         t = ()
-        t = t + (a,)
-        t = t + (b,)
+        t = t + (int(mytuple.split(', ', 1)[0]),)
+        t = t + (int(mytuple.split(', ', 1)[1]),)
         mylist.append(t)
     return mylist
 
-
+# Calculate MST from spicy sparce library and print it to the console
 Tcsr = minimum_spanning_tree(X)
 print 'Minimum Spanning Tree:'
 print Tcsr.toarray().astype(int)
 print Tcsr
 
-sp = shortest_path(X, method='FW', directed=False)
-print 'Shortest Path:'
-print sp
-
+# Draw All connections
 allConnections = getTuples( X )
 g = nx.Graph()
 g.add_edges_from(allConnections)
-pos=nx.spring_layout(g) 
+pos=nx.spring_layout(g)
+# Fixed position, so that nodes are always in the same coordenates
+fixed_positions = {0:(0,0),1:(3,2),2:(7,-1),
+                    3:(1,-3),4:(-3,-2),5:(-6,1),
+                    6:(-3,5),7:(1,6),8:(3,9),9:(6,5)}
+fixed_nodes = fixed_positions.keys()
+pos = nx.spring_layout(g,pos=fixed_positions, fixed = fixed_nodes)
 nx.draw_networkx_nodes(g,pos,node_size=700, node_color="gray")
-nx.draw_networkx_edges(g,pos,width=3,alpha=0.7,edge_color='red')
+nx.draw_networkx_edges(g,pos,width=3,alpha=0.4,edge_color='red')
 nx.draw_networkx_labels(g,pos,font_size=10)
-print(nx.shortest_path(g,source=6,target=2))
-#nx.draw_networkx_labels(g,pos,font_size=10,font_family='sans-serif')
 
-#draw(g)
-#draw_networkx_edges_labels(g,pos=nx.spring_layout(G))
-
+# Draw 2nd Graph - Minimum Spanning Tree
 mst = getTuples(Tcsr)
 g2 = nx.Graph()
 g2.add_edges_from(mst)
@@ -73,7 +68,5 @@ nx.draw_networkx_nodes(g2,pos,node_size=700, node_color="gray")
 nx.draw_networkx_edges(g2,pos,width=3,alpha=0.7,edge_color='green')
 nx.draw_networkx_labels(g2,pos,font_size=10)
 
-
-#draw(g2)
-
+# Show the draw in a window
 show()
--- shortest_path.py
+++ shortest_path.py
@@ -5,74 +5,77 @@ from scipy.sparse.csgraph import shortest_path
 from networkx import draw, Graph
 import networkx as nx 
 import matplotlib.pyplot as plt
-from pylab import show
+from matplotlib.pyplot import show
 import numpy
 import re
 
-a = [0, 289, 549, 195, 331, 206, 220, 233, 333, 383] # GDL
-b = [0, 0, 310, 448, 0, 0, 314, 197, 336, 330]     # MORELIA
-c = [0, 0, 0, 0, 0, 0, 574, 408, 418, 238]        # CDMX
+a = [0, 289, 549, 195, 0, 206, 220, 233, 333, 0] # GDL
+b = [0, 0, 310, 448, 0, 0, 314, 0, 336, 330]     # MORELIA
+c = [0, 0, 0, 0, 0, 0, 574, 408, 418, 238]       # CDMX
 d = [0, 0, 0, 0, 378, 0, 0, 0, 0, 0]             # COLIMA
-e = [0, 0, 0, 0, 0, 162, 0, 0, 0, 0]            # VALLARTA
-f = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]              # TEPIC
-g = [0, 0, 0, 0, 0, 0, 0, 124, 166, 0]         # AGS
-h = [0, 0, 0, 0, 0, 0, 0, 0, 181, 168]       # LEON
-i = [0, 0, 0, 0, 0, 0, 0, 0, 0, 212]       # SLP
-j = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]         # QUERETARO
+e = [0, 0, 0, 0, 0, 162, 0, 0, 0, 0]             # VALLARTA
+f = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]               # TEPIC
+g = [0, 0, 0, 0, 0, 0, 0, 124, 166, 0]           # AGS
+h = [0, 0, 0, 0, 0, 0, 0, 0, 181, 168]           # LEON
+i = [0, 0, 0, 0, 0, 0, 0, 0, 0, 212]             # SLP
+j = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]               # QUERETARO
 
 X = csr_matrix([a, b, c, d, e, f, g, h, i, j])
+fixed_positions = {0:(0,0),1:(3,2),2:(7,-1), 
+                    3:(1,-3),4:(-3,-2),5:(-6,1),
+                    6:(-3,5),7:(1,6),8:(3,9),9:(6,5)}
 
-
+# Get tuples to draw all conections
 def getTuples( mymatrix ):
     str_repr= str( mymatrix )
     mylist = []
     for line in str_repr.splitlines():
         m = re.search('\((.+?)\)', line)
         mytuple = m.group(1)
-    
-        a= int(mytuple.split(', ', 1)[0])
-        b= int(mytuple.split(', ', 1)[1])
         t = ()
-        t = t + (a,)
-        t = t + (b,)
+        t = t + (int(mytuple.split(', ', 1)[0]),)
+        t = t + (int(mytuple.split(', ', 1)[1]),)
         mylist.append(t)
     return mylist
 
-
-
+# Get tuples to draw shortest path
 def get_sp_tuple( myarray ):
-    #print len(myarray)
-    #print myarray[0]
-    #print myarray[1]
     mylist = []
     for i in range(len(myarray)-1):
-        #print i
-        #print myarray[i] 
-        a = myarray[i]
-        b = myarray[i+1]
         t = ()
-        t = t + (a,)
-        t = t + (b,)
+        t = t + (myarray[i],)
+        t = t + (myarray[i+1],)
         mylist.append(t)
     return mylist
 
+# Draw the paths
+def draw_sp( src, trgt, color ):
+    # First draw all Connections
+    all_connections = getTuples(X)
+    g = nx.Graph()
+    g.add_edges_from(all_connections)
+    fixed_nodes = fixed_positions.keys()
+    pos = nx.spring_layout(g,pos=fixed_positions, fixed = fixed_nodes)
+    nx.draw_networkx_nodes(g,pos,node_size=700, node_color="gray")
+    nx.draw_networkx_edges(g,pos,width=3,alpha=0.4,edge_color='red')
+    nx.draw_networkx_labels(g,pos,font_size=10)
+    # Then, get the shortest path
+    sp = nx.shortest_path(g, source=src, target=trgt)
+    sp_tuple = get_sp_tuple(sp)
+    sp_matrix = shortest_path(X, method='FW', directed=False)
+    print 'Shortest Path:'
+    print sp_matrix
+    print sp_tuple
+    g2 = nx.Graph()
+    g2.add_edges_from(sp_tuple)
+    nx.draw_networkx_nodes(g2,pos,node_size=700, node_color="gray")
+    nx.draw_networkx_edges(g2,pos,width=3,alpha=0.7,edge_color=color)
+    nx.draw_networkx_labels(g2,pos,font_size=10)
 
-mst = getTuples(X)
-g = nx.Graph()
-g.add_edges_from(mst)
-pos=nx.spring_layout(g)
-sp = nx.shortest_path(g, source=3, target=9)
-sp_tuple = get_sp_tuple(sp)
-print sp_tuple
-nx.draw_networkx_nodes(g,pos,node_size=700, node_color="gray")
-nx.draw_networkx_edges(g,pos,width=3,alpha=0.4,edge_color='red')
-nx.draw_networkx_labels(g,pos,font_size=10)
-
-g2 = nx.Graph()
-g2.add_edges_from(sp_tuple)
-nx.draw_networkx_nodes(g2,pos,node_size=700, node_color="gray")
-nx.draw_networkx_edges(g2,pos,width=3,alpha=0.7,edge_color='green')
-nx.draw_networkx_labels(g2,pos,font_size=10)
-
-
-show()
+while 1:
+    source = int(input('\nsource: '))
+    target = int(input('\ntarget: '))
+    if source and target < 10:
+        draw_sp(source, target, "black")
+        show()
+        continue
